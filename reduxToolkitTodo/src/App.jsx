78c4f1e706a3e40538f9867fa9058fb0599d0a9e
Dey <biki.dey@hpe.com>
2024-09-03 23:55:58 +0530

    TodoList with redux

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { Provider } from 'react-redux'
+import {store} from './app/store'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+
+function App() {
+  
+
+  return (
+    <Provider store={store}>
+      <h1>Learn about Redux toolkit</h1>
+      <AddTodo />
+      <Todos  />
+    </Provider>
+  )
+}
+
+export default App
